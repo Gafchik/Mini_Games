54d0bbf38fa1480a06b08b1177832292f1c70514
Евгений <78682824+Gafchik@users.noreply.github.com>
2021-02-17 19:02:00 +0200

    переиминовали всё как нужно

--- Mini_Games/Program.cs
+++ Mini_Games/Program.cs
@@ -16,7 +16,7 @@ namespace Mini_Games
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form_main());
         }
     }
 }
