b8f8491c12bf2166ac67b1b449b5b8a325fbdc9f
Евгений <78682824+Gafchik@users.noreply.github.com>
2021-02-17 21:33:16 +0200

    Бюджетный морской бой на минималках готов

--- Mini_Games/Sea_Battle.cs
+++ Mini_Games/Sea_Battle.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,12 +14,14 @@ namespace Mini_Games
     {
         Panel panel_u;
         Panel panel_b;
-        Panel panel_info;
-        Panel panel_info_2;
+        Label panel_info;
+        Label panel_info_2;
         Button exit;
         List<Button> sea_u;
         List<Button> sea_b;
-
+        Random rand_SA;
+        int cout_u ;
+        int cout_b;
         private void Button_Sea_battle_Click(object sender, EventArgs e)
         {
             OFF_Main();
@@ -27,11 +30,12 @@ namespace Mini_Games
             this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
             panel_u = new Panel();
              panel_b = new Panel();
-             panel_info = new Panel();
-            panel_info_2 = new Panel();
+             panel_info = new Label();
+            panel_info_2 = new Label();
             exit = new Button();
              sea_u = new List<Button>();
              sea_b = new List<Button>();
+            rand_SA = new Random();
 
 
             // controls
@@ -47,7 +51,7 @@ namespace Mini_Games
             panel_b.Size = new Size((this.Size.Width / 2) - 50, this.Size.Height / 2+200);
             panel_info.Size = new Size(this.Size.Width / 5, this.Size.Height / 10);
             panel_info_2.Size = new Size(this.Size.Width / 5, this.Size.Height / 10);
-            exit.Size = new Size(this.Size.Width / 5, this.Size.Height / 10);
+            exit.Size = new Size(this.Size.Width / 10, this.Size.Height / 10);
 
 
             //Point
@@ -55,14 +59,18 @@ namespace Mini_Games
             panel_b.Location = new Point((this.Size.Width / 2)+15, 15);
             panel_info.Location = new Point(200, this.Size.Height  - (this.Size.Height / 5) );
             panel_info_2.Location = new Point(this.Size.Width-500, this.Size.Height  - (this.Size.Height / 5) );
-           // exit.Location = new Point(this.Size.Width-500, this.Size.Height  - (this.Size.Height / 5) );
+            exit.Location = new Point(this.Size.Width-770, this.Size.Height  - (this.Size.Height / 5) );
             //Color
             this.BackColor = Color.CadetBlue;
             panel_u.BackColor = Color.Aqua;
             panel_b.BackColor = Color.DarkBlue;
             panel_info.BackColor = Color.Green;
             panel_info_2.BackColor = Color.Red;
-            exit.BackColor = Color.Blue;
+            exit.BackColor = Color.Gray;
+
+            exit.Text = "ВЫХОД";
+            exit.Click += Exit_Click;
+
 
             // sea
             for (int i = 0; i < 100; i++)
@@ -80,8 +88,8 @@ namespace Mini_Games
             sea_u.ForEach(i => i.Size = new Size(50, 50));
             sea_b.ForEach(i => i.Size = new Size(50, 50));
             // color
-            sea_u.ForEach(i => i.BackColor = Color.Gray);
-            sea_b.ForEach(i => i.BackColor = Color.Gray);
+            sea_u.ForEach(i => i.BackColor = Color.Silver);
+            sea_b.ForEach(i => i.BackColor = Color.Silver);
             // позиция
             for (int i = 0; i < sea_u.Count; i++)
             {
@@ -104,29 +112,120 @@ namespace Mini_Games
                     }
                 }
             }
-           
-         
 
-                // подвязка на клик
-                sea_u.ForEach(i => i.Click += sea_u_Click);
-            sea_b.ForEach(i => i.Click += sea_u_Click);
+
+
+            // подвязка на клик
+           // sea_u.ForEach(i => i.Click += sea_u_Click);
+            sea_b.ForEach(i => i.Click += sea_b_Click);
             // значение по умолчаниею
             sea_u.ForEach(i => i.Name = "ok");
             sea_b.ForEach(i => i.Name = "ok");
             
             // корабли
-           /* for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
-                sea_u[rand_sapp.Next(0, sea_u.Count - 1)].Name = "*";
-                sea_b[rand_sapp.Next(0, sea_b.Count - 1)].Name = "*";
-            } */
-                  
+                int j = rand_SA.Next(0, 99);
+                sea_u[j].Name = "000";
+                sea_u[j].Text = sea_u[j].Name;
+                sea_b[j].Name = "000";
+                
+            }
 
-          
+
+             cout_u = sea_u.Count(i => i.Name == "000");
+            cout_b = sea_b.Count(i => i.Name == "000");
         }
-        private void sea_u_Click(object sender, System.EventArgs e)
+
+        private void Exit_Click(object sender, EventArgs e)
         {
-
+            FormBorderStyle = FormBorderStyle.Sizable;
+            BackColor = Color.White;
+            panel_u.Dispose();
+            panel_b.Dispose();
+            panel_info.Dispose();
+            panel_info_2.Dispose();
+            exit.Dispose();
+            sea_u.ForEach(i => i.Dispose());
+            sea_b.ForEach(i => i.Dispose());
+            ON_Main();
         }
+
+        private void sea_b_Click(object sender, System.EventArgs e)
+        {
+            int j = rand_SA.Next(0, 99);
+            int index = sea_b.IndexOf((sender as Button));
+            if ((sender as Button).Name == "000")
+            {
+
+                sea_b[index].Name = "УБИТ";
+                sea_b[index].Text = "УБИТ";
+                sea_b[index].Enabled = false;             
+
+            }
+            else
+            {
+                sea_b[index].Text = "XXX\nXXX\nXXX";
+                sea_b[index].Enabled = false;
+            }
+
+            while (!sea_u[j].Enabled)
+            {
+                j = rand_SA.Next(0, 99);
+            }
+            if (sea_u[j].Name=="000")
+            {
+                sea_u[j].Name = "УБИТ";
+                sea_u[j].Text = "УБИТ";
+                sea_u[j].Enabled = false;
+            }
+            else
+            {
+                sea_u[j].Text = "XXX\nXXX\nXXX";
+                sea_u[j].Enabled = false;
+            }
+             cout_u = sea_u.Count(i => i.Name == "000");
+            cout_b =  sea_b.Count(i => i.Name == "000");
+            panel_info.Text = $"Твоих кораблей осталось {cout_u}";
+            panel_info_2.Text = $"Кораблей бота осталось {cout_b}";
+            if(cout_u==0 || cout_b == 0)
+            {
+                DialogResult dialog = MessageBox.Show(" Повторим ?", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialog == DialogResult.No)
+                {
+                    FormBorderStyle = FormBorderStyle.Sizable;
+                    BackColor = Color.White;
+                    panel_u.Dispose();
+                    panel_b.Dispose();
+                    panel_info.Dispose();
+                    panel_info_2.Dispose();
+                    exit.Dispose();
+                    sea_u.ForEach(i => i.Dispose());
+                    sea_b.ForEach(i => i.Dispose());
+                    ON_Main();
+                }
+                else
+                {
+                    count_open = 0;
+
+                    sea_u.ForEach(i => i.Name = "ok");
+                    sea_b.ForEach(i => i.Name = "ok");
+                    sea_u.ForEach(i => i.Text = "");
+                    sea_b.ForEach(i => i.Text = "");
+                    sea_u.ForEach(i => i.Enabled = true);
+                    sea_b.ForEach(i => i.Enabled = true);
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int q = rand_SA.Next(0, 99);
+                        sea_u[q].Name = "000";
+                        sea_u[q].Text = sea_u[q].Name;
+                        sea_b[q].Name = "000";
+
+                    }
+
+                }
+            }
+        }
+      
     }
 }
