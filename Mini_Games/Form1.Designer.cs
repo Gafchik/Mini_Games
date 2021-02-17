d4c73da41ef75c8088f83be4576903c5cdca1053
Евгений <78682824+Gafchik@users.noreply.github.com>
2021-02-17 19:04:20 +0200

    добавили саппера

--- Mini_Games/Form1.Designer.cs
+++ Mini_Games/Form1.Designer.cs
@@ -1,8 +1,13 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
 namespace Mini_Games
 {
     partial class Form_main
     {
+
         /// <summary>
         /// Обязательная переменная конструктора.
         /// </summary>
@@ -21,7 +26,6 @@ namespace Mini_Games
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
         /// Требуемый метод для поддержки конструктора — не изменяйте 
@@ -29,20 +33,185 @@ namespace Mini_Games
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            #region Form_main
+            this.AutoScaleDimensions = new SizeF(6F, 13F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.BackColor = Color.White;
+            this.Size = new Size(500, 500);
+            this.Name = "Form_main";
+            this.Text = "Main";
             this.ResumeLayout(false);
+            this.PerformLayout();
+            this.SizeChanged += Form_main_SizeChanged;
+            #endregion
+
+            #region new Memory
+
+            this.main_lble = new Label();
+            this.button_X_O = new Button();
+            this.button_Sea_battle = new Button();
+            this.button_Sapper = new Button();
+            this.SuspendLayout();
+            #endregion
+
+            #region main_lble
+
+            main_lble.BackColor = Color.Black;
+            main_lble.Enabled = true;
+            main_lble.Font = new Font("Microsoft Sans Serif", 40F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204)));
+            main_lble.ForeColor = Color.Yellow;
+            main_lble.Location = new Point(0, 0);
+            main_lble.Name = "main_lble";
+            main_lble.Size = new Size(this.Size.Width, (this.Size.Height / 4) - 10);
+            main_lble.TabIndex = 0;
+            main_lble.Text = "MINI GAMES";
+            main_lble.TextAlign = ContentAlignment.MiddleCenter;
+
+            #endregion
+
+            #region button_X_O
+
+            button_X_O.BackColor = Color.Yellow;
+            button_X_O.Cursor = Cursors.Hand;
+            button_X_O.Font = new Font("Microsoft Sans Serif", 40F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204)));
+            button_X_O.Location = new Point(0, main_lble.Size.Height);
+            button_X_O.Size = new Size(this.Size.Width, (this.Size.Height / 4) - 10);
+            button_X_O.Name = "button_X_O";
+            button_X_O.TabIndex = 1;
+            button_X_O.Text = "X vs O";
+            button_X_O.Click += Button_X_O_Click;
+            button_X_O.MouseHover += Button_Main_MouseHover;
+            button_X_O.MouseLeave += Button_Main_MouseLeave;
+
+            #endregion
+
+            #region button_Sea_battle
+
+            button_Sea_battle.BackColor = Color.Yellow;
+            button_Sea_battle.Cursor = Cursors.Hand;
+            button_Sea_battle.Font = new Font("Microsoft Sans Serif", 40F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204)));
+            button_Sea_battle.Location = new Point(0, main_lble.Size.Height + button_X_O.Size.Height);
+            button_Sea_battle.Size = new Size(this.Size.Width, (this.Size.Height / 4) - 10);
+            button_Sea_battle.Name = "button_Sea_battle";
+            button_Sea_battle.TabIndex = 2;
+            button_Sea_battle.Text = "Sea Battle";
+            button_Sea_battle.MouseHover += Button_Main_MouseHover;
+            button_Sea_battle.MouseLeave += Button_Main_MouseLeave;
+
+            #endregion
+
+            #region button_Sapper
+
+            button_Sapper.BackColor = Color.Yellow;
+            button_Sapper.Cursor = Cursors.Hand;
+            button_Sapper.Font = new Font("Microsoft Sans Serif", 40F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204)));
+            button_Sapper.Location = new Point(0, main_lble.Size.Height + button_X_O.Size.Height + button_Sea_battle.Size.Height);
+            button_Sapper.Size = new Size(this.Size.Width, (this.Size.Height / 4) - 10);
+            button_Sapper.Name = "button_Sapper";
+            button_Sapper.TabIndex = 3;
+            button_Sapper.Text = "Sapper";
+            button_Sapper.MouseHover += Button_Main_MouseHover;
+            button_Sapper.MouseLeave += Button_Main_MouseLeave;
+            button_Sapper.Click += Button_Sapper_Click;
+
+            #endregion
+
+            #region Добавление в контрольку
+
+            this.Controls.Add(button_Sapper);
+            this.Controls.Add(button_Sea_battle);
+            this.Controls.Add(button_X_O);
+            this.Controls.Add(main_lble);
+
+            #endregion
 
         }
 
+
+
+
+
+
+
+
+
+        #region ивент изменения размера формы
+        private void Form_main_SizeChanged(object sender, System.EventArgs e)
+        {
+            #region позиция Main
+            main_lble.Size = new Size(this.Size.Width, (this.Size.Height / 4) - 10);
+
+            button_X_O.Location = new Point(0, main_lble.Size.Height);
+            button_X_O.Size = new Size(this.Size.Width, (this.Size.Height / 4) - 10);
+
+            button_Sea_battle.Location = new Point(0, main_lble.Size.Height + button_X_O.Size.Height);
+            button_Sea_battle.Size = new Size(this.Size.Width, (this.Size.Height / 4) - 10);
+
+            button_Sapper.Location = new Point(0, main_lble.Size.Height + button_X_O.Size.Height + button_Sea_battle.Size.Height);
+            button_Sapper.Size = new Size(this.Size.Width, (this.Size.Height / 4) - 10);
+
+
+
+            #endregion
+
+            #region позиция крестики нолики
+            if (buttons_X_O != null)
+            {
+                buttons_X_O.ForEach(i => i.Size = new Size((this.Size.Width / 3) - 7, (this.Size.Height / 3) - 14));
+                for (int i = 0; i < buttons_X_O.Count; i++)
+                {
+                    if (i == 0)
+                        buttons_X_O[i].Location = new Point(0, 0);
+                    else
+                    {
+                        if (buttons_X_O[i - 1].Location.X >= this.Size.Width - buttons_X_O[i - 1].Location.X)
+                        {
+                            buttons_X_O[i].Location = new Point(0, buttons_X_O[i - 1].Location.Y + buttons_X_O[i - 1].Size.Height);
+                        }
+                        else
+                            buttons_X_O[i].Location = new Point(buttons_X_O[i - 1].Location.X + buttons_X_O[i - 1].Size.Width, buttons_X_O[i - 1].Location.Y);
+
+                    }
+                }
+            }
+
+            #endregion
+        }
         #endregion
+        private void Button_Main_MouseLeave(object sender, System.EventArgs e)
+        {
+            ((Button)sender).BackColor = Color.Yellow;
+        }
+
+        private void Button_Main_MouseHover(object sender, System.EventArgs e)
+        {
+            ((Button)sender).BackColor = Color.Red;
+        }
+        private void OFF_Main()
+        {
+            button_Sapper.Visible = false;
+            button_Sea_battle.Visible = false;
+            button_X_O.Visible = false;
+            main_lble.Visible = false;
+        }
+        private void ON_Main()
+        {
+            button_Sapper.Visible = true;
+            button_Sea_battle.Visible = true;
+            button_X_O.Visible = true;
+            main_lble.Visible = true;
+        }
+
+        #region переменные
+
+        private Label main_lble;
+        private Button button_X_O;
+        private Button button_Sea_battle;
+        private Button button_Sapper;
+
+        #endregion
+
     }
 }
+
 
---
+++ Mini_Games/Sapper.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mini_Games
+{
+    public partial class Form_main
+    {
+        private Panel panel_game;
+        private List<Button> minefield;
+        private PictureBox output;
+
+        private Label top_lable;
+        private PictureBox close;
+        private int count_mine;
+        private int count_field;
+        private int count_open;
+        private Random rand_sapp;
+
+        private void Button_Sapper_Click(object sender, EventArgs e)
+        {
+            OFF_Main();
+
+            #region выдиление памяти
+            count_mine = 0;
+            count_open = 0;
+            rand_sapp = new Random();
+            count_field = 100;
+            minefield = new List<Button>();
+            top_lable = new Label();
+            output = new PictureBox();
+            panel_game = new Panel();
+
+            close = new PictureBox();
+            close.Image = Image.FromFile($"{Directory.GetCurrentDirectory()}\\Img\\close.png");
+
+            SuspendLayout();
+
+            #endregion
+
+            #region output
+
+            output.Cursor = Cursors.Hand;
+            output.Image = close.Image;
+            output.SizeMode = PictureBoxSizeMode.StretchImage;
+            output.Size = new Size(this.Size.Width / 5, this.Size.Width / 15);
+            output.Location = new Point(this.Size.Width - (this.Size.Width / 3), (this.Size.Height / 2) + (this.Size.Height / 4) + (this.Size.Height / 15));
+            output.Name = "output";
+            output.TabIndex = 4;
+            output.Click += Output_Click;
+
+            #endregion
+
+
+            #region top_panel
+
+            top_lable.BackColor = Color.BlueViolet;
+            top_lable.Location = new Point(0, 0);
+            top_lable.Name = "top_panel";
+            top_lable.Size = new Size(this.Size.Width, this.Size.Height / 5);
+            top_lable.TabIndex = 0;
+            top_lable.Text = $"Oткрытых полей = {count_open}\nСкрытых мин на карте = {count_mine}";
+            top_lable.Font = new Font("Tobota", 20, FontStyle.Bold);
+            top_lable.TextAlign = ContentAlignment.MiddleCenter;
+
+            #endregion
+
+            #region    panel_game
+
+            panel_game.BackColor = Color.White;
+            panel_game.Location = new Point(0, (this.Size.Height / 5) - 9);
+            panel_game.Size = new Size(this.Size.Width, this.Size.Height - top_lable.Size.Height - close.Size.Height - (this.Size.Height / 7));
+            panel_game.Name = "panel_game";
+            panel_game.TabIndex = 2;
+
+            #endregion
+
+
+
+
+
+            #region   Form_main
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            BackColor = Color.BlueViolet;
+            Controls.Add(output);
+            Size = new Size(500, 500);
+            Controls.Add(panel_game);
+            Controls.Add(top_lable);
+            ResumeLayout(false);
+
+            #endregion
+
+
+            #region minefield
+            // добавление
+            for (int i = 0; i < count_field; i++)
+            {
+                minefield.Add(new Button());
+            }
+            // в игровую панель
+            minefield.ForEach(i => panel_game.Controls.Add(i));
+            // размер
+            minefield.ForEach(i => i.Size = new Size((panel_game.Size.Width / 10) - 2, (panel_game.Size.Height / 10)));
+            // позиция
+            for (int i = 0; i < minefield.Count; i++)
+            {
+                if (i == 0)
+                    minefield[i].Location = new Point(0, 0);
+                else
+                {
+                    if (i % 10 == 0)
+                    {
+                        minefield[i].Location = new Point(0, minefield[i - 1].Location.Y + minefield[i - 1].Size.Height);
+                    }
+                    else
+                        minefield[i].Location = new Point(minefield[i - 1].Location.X + minefield[i - 1].Size.Width, minefield[i - 1].Location.Y);
+                }
+            }
+            // подвязка на клик
+            minefield.ForEach(i => i.Click += minefield_Click);
+            // значение по умолчаниею
+            minefield.ForEach(i => i.Name = "ok");
+            // minefield.ForEach(i => i.Text = minefield.FindIndex(j => j.GetHashCode() == i.GetHashCode()).ToString() );
+            // минирование
+            for (int i = 0; i < minefield.Count / 5; i++)
+            {
+                minefield[rand_sapp.Next(0, minefield.Count - 1)].Name = "0";
+            }
+            count_mine = minefield.Count(i => i.Name == "0");
+            #endregion
+
+            minefield.ForEach(i => i.BackColor = Color.Gray);
+
+        }
+
+
+
+        private void minefield_Click(object sender, EventArgs e)
+        {
+            if (((Button)sender).Name == "0")
+            {
+
+                for (int i = 0; i < count_field; i++)
+                {
+                    if (minefield[i].Name == "0")
+                    {
+                        minefield[i].Text = minefield[i].Name;
+                        minefield[i].BackColor = Color.Red;
+                    }
+                }
+
+                DialogResult dialog = MessageBox.Show("Бабах!\nТы все просрал, повторим ?", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialog == DialogResult.No)
+                {
+                    FormBorderStyle = FormBorderStyle.Sizable;
+                    BackColor = Color.White;
+                    panel_game.Dispose();
+                    output.Dispose();
+                    top_lable.Dispose();
+                    close.Dispose();
+                    ON_Main();
+                }
+                else
+                {
+                    count_open = 0;
+                    minefield.ForEach(i => i.BackColor = Color.Gray);
+                    minefield.ForEach(i => i.Name = "ok");
+                    minefield.ForEach(i => i.Text = "");
+                    minefield.ForEach(i => i.Enabled = true);
+                    for (int i = 0; i < minefield.Count / 5; i++)
+                    {
+                        minefield[rand_sapp.Next(0, minefield.Count - 1)].Name = "0";
+                    }
+                    count_mine = minefield.Count(i => i.Name == "0");
+
+                }
+
+            }
+            else
+            {
+                ((Button)sender).Text = ((Button)sender).Name;
+                ((Button)sender).Enabled = false;
+                ((Button)sender).BackColor = Color.Green;
+                count_open++;
+
+                int cout_i_rand = rand_sapp.Next(3, 25);
+                if (cout_i_rand >= minefield.Count(i => i.Enabled == true))
+                    cout_i_rand = 0;
+
+                for (int i = 0; i < cout_i_rand; i++)
+                {
+                    int temp_rand = rand_sapp.Next(0, 99);
+                    if (minefield[temp_rand].Enabled == true)
+                    {
+                        if (minefield[temp_rand].Name == "0")
+                        {
+                            minefield[temp_rand].Text = minefield[temp_rand].Name;
+                            minefield[temp_rand].BackColor = Color.Red;
+                            count_open++;
+                            count_mine--;
+                        }
+                        else
+                        {
+                            minefield[temp_rand].Text = minefield[temp_rand].Name;
+                            minefield[temp_rand].BackColor = Color.Green;
+                            count_open++;
+                        }
+                        minefield[temp_rand].Enabled = false;
+                    }
+                    else
+                        i--;
+                }
+                top_lable.Text = $"Oткрытых полей = {count_open}\nСкрытых мин на карте = {count_mine}";
+                if (count_open == 100 || count_mine == 0)
+                {
+
+                    DialogResult dialog = MessageBox.Show("ЛЯ ты молодец)\n повторим ?", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (dialog == DialogResult.No)
+                    {
+                        FormBorderStyle = FormBorderStyle.Sizable;
+                        BackColor = Color.White;
+                        panel_game.Dispose();
+                        output.Dispose();
+                        top_lable.Dispose();
+                        close.Dispose();
+                        ON_Main();
+                    }
+                    else
+                    {
+                        count_open = 0;
+                        minefield.ForEach(i => i.BackColor = Color.Gray);
+                        minefield.ForEach(i => i.Name = "ok");
+                        minefield.ForEach(i => i.Text = "");
+                        minefield.ForEach(i => i.Enabled = true);
+                        for (int i = 0; i < minefield.Count / 5; i++)
+                        {
+                            minefield[rand_sapp.Next(0, minefield.Count - 1)].Name = "0";
+                        }
+                        count_mine = minefield.Count(i => i.Name == "0");
+
+                    }
+                }
+            }
+            top_lable.Text = $"Oткрытых полей = {count_open}\nСкрытых мин на карте = {count_mine}";
+        }
+
+        private void Output_Click(object sender, EventArgs e)
+        {
+
+            BackColor = Color.White;
+            panel_game.Dispose();
+            output.Dispose();
+            top_lable.Dispose();
+            close.Dispose();
+            ON_Main();
+        }
+    }
+}
